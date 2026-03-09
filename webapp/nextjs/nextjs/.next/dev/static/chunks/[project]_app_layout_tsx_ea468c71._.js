5b678217010f9dd28b021b95dcb8dc67d8cc2b4f
S. Kamakura <doburoi0114@gmail.com>
2026-03-09 19:13:57 +0900

    Delete webapp/nextjs/nextjs/.next/dev/static directory

--- webapp/nextjs/nextjs/.next/dev/static/chunks/[project]_app_layout_tsx_ea468c71._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__f12f3c47._.js",
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css"
-],
-    source: "dynamic"
-});
